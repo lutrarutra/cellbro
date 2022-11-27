b9d839009c308411046015842a11c671816254be
lutrarutra <artur.gynter@hotmail.com>
2022-11-27 19:37:04 +0200

    fixed scaling

--- cellbro/core/pages/qc.py
+++ cellbro/core/pages/qc.py
@@ -1,20 +1,20 @@
 import dash
 from dash import html, dcc, Input, Output, State
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
 
 from cellbro.plotting.QC import QC
 
 def create_page(dash_app, dataset):
-
-    bottom_left_sidebar, bottom_figure = QC.create_layout(dataset)
+    top_sidebar, main_figure, bottom_sidebar, bottom_figure = QC.create_layout(dataset)
 
     layout = [
         html.Div(id="top", className="top", children=[
-            
+            top_sidebar, main_figure,
             ]),
         html.Div(id="bottom", className="bottom", children=[
-            bottom_left_sidebar, bottom_figure
+            bottom_sidebar, bottom_figure
         ])
     ]
 
@@ -22,8 +22,17 @@ def create_page(dash_app, dataset):
         output=QC.get_callback_outputs(),
         inputs=QC.get_callback_inputs(),
     )
-    def _(submit):
-        return QC(dataset).plot()
+    def _(**kwargs):
+        return QC(dataset, kwargs).plot()
+
+    outputs, inputs, states = QC.get_filtering_callbacks()
+    @dash_app.callback(
+        output=outputs,
+        inputs=inputs,
+        state=states,
+    )
+    def _(submit, **kwargs):
+        return QC(dataset, kwargs).filter(submit)
 
     dash.register_page("pages.qc", title="QC", path="/qc", order=1, layout=layout)
 
--- cellbro/plotting/QC.py
+++ cellbro/plotting/QC.py
@@ -13,21 +13,36 @@ import numpy as np
 figure_layout = go.Layout(
     paper_bgcolor='white',
     plot_bgcolor='white',
-    xaxis=dict(showgrid=False, zeroline=False, visible=False, showticklabels=True),
+    xaxis=dict(showgrid=False, zeroline=False, visible=True, showticklabels=True),
     yaxis=dict(showgrid=False, zeroline=False, visible=True, showticklabels=True),
     margin=dict(t=0, b=0, l=0, r=0),
 )
 
+qc_params = ParamsDict([
+    Param(key="pct_counts_mt", name="Max MT%", default=5.0, type=float, description="", step=1),
+    Param(key="min_genes", name="Min. Genes (per cell)", default=200, type=int, description="", step=100),
+    Param(key="min_cells", name="Min. Cells (per gene)", default=10, type=int, description="", step=10),
+])
+
 class QC():
-    def __init__(self, dataset):
+    def __init__(self, dataset, params):
         self.dataset = dataset
+        self.params = params
 
-    def plot(self):
-        fig = make_subplots(rows=1, cols=3)
+    def filter(self, submit):
+        # Makes sure that filtering is not done on initial load
+        if submit is None:
+            return list(self.params.values())
+        print("Filtering...")
+        self.dataset.adata = self.dataset.adata[self.dataset.adata.obs.pct_counts_mt < self.params["pct_counts_mt"], :].copy()
+        sc.pp.filter_cells(self.dataset.adata, min_genes=self.params["min_genes"])
+        sc.pp.filter_genes(self.dataset.adata, min_cells=self.params["min_cells"])
+        return list(self.params.values())
 
+    def violin_plot(self):
         violins = ["n_genes_by_counts", "total_counts", "pct_counts_mt"]
+        fig = make_subplots(rows=1, cols=3)
         x = self.dataset.adata.obs[violins]
-
         df = pd.DataFrame(
             x, columns=violins
         )
@@ -35,8 +50,8 @@ class QC():
         for i, feature in enumerate(violins):
             fig.add_trace(
                     go.Violin(
+                        name=feature.replace("_", " ").title(),
                         y=df[feature],
-                        line_color=sc.pl.palettes.default_20[i],
                         box_visible=True,
                         points="all",
                         pointpos=0,
@@ -44,31 +59,82 @@ class QC():
                         jitter=0.6,
                 ), row=1, col=i+1
             )
-            fig["layout"][f"xaxis{i+1 if i != 0 else ''}"]["title"] = feature.replace("_", " ").title()
 
         fig.update_layout(figure_layout)
+        fig.update_layout(showlegend=False)
 
-        return [fig]
+        return fig
     
+    def mt_plot(self):
+        color = (self.dataset.adata.obs["pct_counts_mt"] > self.params["pct_counts_mt"]).values
         
+        cmap = ["#d3d3d3", sc.pl.palettes.default_20[0]] if color[0] else [sc.pl.palettes.default_20[0], "#d3d3d3"]
+
+        fig = px.scatter(
+            self.dataset.adata.obs,
+            x="total_counts",
+            y="pct_counts_mt",
+            color=color,
+            color_discrete_sequence=cmap,
+        )
+        fig.update_layout(figure_layout)
+        fig.update_layout(xaxis_title="Total Counts", yaxis_title="MT%", legend_title_text=f"MT > {self.params['pct_counts_mt']:.1f}%")
+        fig.add_hline(y=self.params["pct_counts_mt"], line_width=1, line_dash="dash", line_color=sc.pl.palettes.default_20[3])
+        return fig
+
+    def plot(self):
+        main_figure = self.mt_plot()
+        bottom_figure = self.violin_plot()
+
+        return [main_figure, bottom_figure]
+
+    @staticmethod
+    def get_filtering_callbacks():
+        outputs = []
+        for param in qc_params.values():
+            outputs.append(Output(component_id=f"qc-{param.key}", component_property="value"))
+
+        inputs = {
+            "submit": Input(component_id="filtering-submit", component_property="n_clicks"),
+        }
+        states = {}
+        for param in qc_params.values():
+            states[param.key] = State(component_id=f"qc-{param.key}", component_property="value")
+        return outputs, inputs, states
+    
     @staticmethod
     def get_callback_outputs():
         return [
+            Output(component_id="mt-plot", component_property="figure"),
             Output(component_id="qc-violin-plot", component_property="figure"),
         ]
         
     # Inputs to Projection
     @staticmethod
     def get_callback_inputs():
-        return {
-            "submit": Input(component_id="qc-submit", component_property="n_clicks"),
-            # "feature": Input(component_id="qc-violin-feature", component_property="value"),
-            # "groupby": Input(component_id="violin-groupby", component_property="value"),
-        }
+        inputs = {}
+        for param in qc_params.values():
+            inputs[param.key] = Input(component_id=f"qc-{param.key}", component_property="value")
+        return inputs
 
     @staticmethod
     def create_layout(dataset):
-        sidebar = html.Div(children=[
+        top_sidebar = html.Div(children=[
+            html.Div([
+                html.H3("Filtering Settings"),
+            ], className="top-header"),
+            dcc.Loading(type="circle", children=[
+                html.Div(children=[
+                    QC.params_layout(),
+                ], className="top-parameters"),
+                html.Div([
+                    dbc.Button("Filter", color="primary", className="mr-1", id="filtering-submit"),
+                ], className="top-footer")
+            ],),
+        ], className="top-sidebar")
+
+
+        bottom_sidebar = html.Div(children=[
             dcc.Loading(type="circle", children=[
                 html.Div([
                     dbc.Button("Plot", color="primary", className="mr-1", id="qc-submit"),
@@ -76,11 +142,35 @@ class QC():
             ],)
         ], id="qc-violin-sidebar", className="bottom-sidebar")
 
-        figure = html.Div(children=[
+        main_figure = html.Div(children=[
+            html.Div([
+                dcc.Loading(
+                    id="loading-mt", type="circle",
+                    children=[html.Div(dcc.Graph(id="mt-plot", className="main-plot"))],
+                )
+            ], id="mt-figure", className="main-figure")
+        ], className="main")
+
+        bottom_figure = html.Div(children=[
             dcc.Loading(
                 id="loading-qc-violin", className="loading-bottom", type="circle",
                 children=[html.Div(dcc.Graph(id="qc-violin-plot", className="bottom-plot"))],
             )
         ], id="qc-violin-figure", className="bottom-figure")
 
-        return sidebar, figure
+        return top_sidebar, main_figure, bottom_sidebar, bottom_figure
+
+    @staticmethod
+    def params_layout():
+        layout = html.Div(children=[
+            html.Div(children=[
+                html.Label(
+                    param.name, className="param-label",
+                ),
+                dcc.Input(
+                    id=f"qc-{key}", type=param.input_type, value=param.value,
+                    step=param.step if param.step != None else 0.1, className="param-input",
+                ),
+            ], className="param-row") for key, param in qc_params.items()
+        ])
+        return layout
