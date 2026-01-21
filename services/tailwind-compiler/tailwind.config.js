c992927451f7f78eb85dbe1443b5b8d0288a66ee
lutrarutra <artur.gynter@hotmail.com>
2026-01-21 16:54:16 +0100

    tailwind + daisy

--- services/tailwind-compiler/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["/app/templates/**/*.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
-}
